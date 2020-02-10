47e4b67ea38cb056819f522b9294db7a8ddc9b89
Rishabh Kr. Agarwal <agarwal.rishabh100@gmail.com>
2020-02-10 19:38:20 +0530

    Add files via upload

---
+++ qp.py
@@ -0,0 +1,17 @@
+def mat(s, d):
+    (s_x, s_y), (d_x, d_y) = s, d
+    route = set()
+    for i in range( s_x , d_x+(1 if s_x<=d_x else -1) , (1 if s_x<=d_x else -1) ):
+        if (i,s_y) not in route:
+            route.add( (i,s_y) )
+            print( (i,s_y) )
+
+    for i in range( s_y , d_y+(1 if s_y<=d_y else -1) , (1 if s_y<=d_y else -1) ):
+        if (d_x,i) not in route:
+            route.add( (d_x,i) )
+            print( (d_x,i) )
+
+
+
+print('')
+mat((1,1),(1,6))
