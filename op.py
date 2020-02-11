c35ec594e1fcaf627f3e72b99a0131ae5bd97978
Rishabh Kr. Agarwal <agarwal.rishabh100@gmail.com>
2020-02-11 18:00:05 +0530

    Update op.py

--- op.py
+++ op.py
@@ -10,7 +10,7 @@ def findstem(arr):
                 res = stem
     return res
 
-arr = ["awmfe", "ame", "tame"]
+arr = ["awmfe", "ame", "bame"]
 common_substr = findstem(arr) 
 if len(common_substr)>1:
     print(common_substr)
